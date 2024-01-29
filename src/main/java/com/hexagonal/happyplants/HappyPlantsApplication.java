55c2e7a5df1cd3adecc4430a80464f9bcc183a04
RodrigoTopan <rodrigo.topan.ti@gmail.com>
2024-01-29 19:12:40 -0300

    feat: initial commit

---
+++ src/main/java/com/hexagonal/happyplants/HappyPlantsApplication.java
@@ -0,0 +1,13 @@
+package com.hexagonal.happyplants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyPlantsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyPlantsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hexagonal/happyplants/HappyPlantsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexagonal.happyplants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyPlantsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
